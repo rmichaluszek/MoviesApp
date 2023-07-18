7063a7ce93770372058b62521bfa575592e45463
Rafał Michałuszek <rmichaluszek@gmail.com>
2023-07-18 22:08:09 +0200

    Moved whole project to next.js

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'MoviesApp',
+  description: 'Simple project in Next.js + React.js that uses TMDb API',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
